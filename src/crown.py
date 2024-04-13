22ca0e84ca374abfdfafb4c3acd0f36c46bd1bc8
t1u4n <luan.tian@outlook.com>
2024-04-13 16:04:13 -0400

    Modify CROWN algorithm for l1-norm perturbation

--- src/crown.py
+++ src/crown.py
@@ -34,7 +34,7 @@ class BoundedSequential(nn.Sequential):
                 layers.append(BoundSimplexNeuron.convert(l))
         return BoundedSequential(*layers)
 
-    def compute_bounds(self, x_U=None, x_L=None, upper=True, lower=True, optimize=False):
+    def compute_bounds(self, x=None, upper=True, lower=True, optimize=False, eps=1.):
         r"""Main function for computing bounds.
 
         Args:
@@ -54,10 +54,10 @@ class BoundedSequential(nn.Sequential):
             lb (tensor): The lower bound of the final output.
         """
         ub = lb = None
-        ub, lb = self.full_boundpropogation(x_U=x_U, x_L=x_L, upper=upper, lower=lower)
+        ub, lb = self.full_boundpropogation(x=x, upper=upper, lower=lower, eps=eps)
         return ub, lb
 
-    def full_boundpropogation(self, x_U=None, x_L=None, upper=True, lower=True):
+    def full_boundpropogation(self, x=None, upper=True, lower=True, eps=1.):
         r"""A full bound propagation. We are going to sequentially compute the
         intermediate bounds for each linear layer followed by a ReLU layer. For each
         intermediate bound, we call self.boundpropogate_from_layer() to do a bound propagation
@@ -84,20 +84,20 @@ class BoundedSequential(nn.Sequential):
             if isinstance(modules[i], BoundReLU):
                 if isinstance(modules[i - 1], BoundLinear):
                     # add a batch dimension
-                    newC = torch.eye(modules[i - 1].out_features).unsqueeze(0).repeat(x_U.shape[0], 1, 1).to(x_U)
+                    newC = torch.eye(modules[i - 1].out_features).unsqueeze(0).repeat(x.shape[0], 1, 1).to(x)
                     # Use CROWN to compute pre-activation bounds
                     # starting from layer i-1
-                    ub, lb = self.boundpropogate_from_layer(x_U=x_U, x_L=x_L, C=newC, upper=True, lower=True,
-                                                            start_node=i - 1)
+                    ub, lb = self.boundpropogate_from_layer(x=x, C=newC, upper=True, lower=True,
+                                                            start_node=i - 1, eps=eps)
                 # Set pre-activation bounds for layer i (the ReLU layer)
                 modules[i].upper_u = ub
                 modules[i].lower_l = lb
         # Get the final layer bound
-        return self.boundpropogate_from_layer(x_U=x_U, x_L=x_L,
-                                              C=torch.eye(modules[i].out_features).unsqueeze(0).to(x_U), upper=upper,
-                                              lower=lower, start_node=i)
+        return self.boundpropogate_from_layer(x=x,
+                                              C=torch.eye(modules[i].out_features).unsqueeze(0).to(x), upper=upper,
+                                              lower=lower, start_node=i, eps=eps)
 
-    def boundpropogate_from_layer(self, x_U=None, x_L=None, C=None, upper=False, lower=True, start_node=None):
+    def boundpropogate_from_layer(self, x=None, C=None, upper=False, lower=True, start_node=None, eps=1.):
         r"""The bound propagation starting from a given layer. Can be used to compute intermediate bounds or the final bound.
 
         Args:
@@ -120,32 +120,30 @@ class BoundedSequential(nn.Sequential):
         modules = list(self._modules.values()) if start_node is None else list(self._modules.values())[:start_node + 1]
         upper_A = C if upper else None
         lower_A = C if lower else None
-        upper_sum_b = lower_sum_b = x_U.new([0])
+        upper_sum_b = lower_sum_b = x.new([0])
         for i, module in enumerate(reversed(modules)):
             upper_A, upper_b, lower_A, lower_b = module.boundpropogate(upper_A, lower_A, start_node)
             upper_sum_b = upper_b + upper_sum_b
             lower_sum_b = lower_b + lower_sum_b
 
         # sign = +1: upper bound, sign = -1: lower bound
-        def _get_concrete_bound(A, sum_b, sign=-1):
+        def _get_concrete_bound(A: torch.Tensor, sum_b, x, sign=-1):
             if A is None:
                 return None
             A = A.view(A.size(0), A.size(1), -1)
             # A has shape (batch, specification_size, flattened_input_size)
-            x_ub = x_U.view(x_U.size(0), -1, 1)
-            x_lb = x_L.view(x_L.size(0), -1, 1)
-            center = (x_ub + x_lb) / 2.0
-            diff = (x_ub - x_lb) / 2.0
-            bound = A.bmm(center) + sign * A.abs().bmm(diff)
+            x = x.reshape(x.shape[0], -1, 1)
+            deviation = A.norm(np.inf, -1) * eps
+            bound = A.matmul(x) + sign * deviation.unsqueeze(-1)
             bound = bound.squeeze(-1) + sum_b
             return bound
 
-        lb = _get_concrete_bound(lower_A, lower_sum_b, sign=-1)
-        ub = _get_concrete_bound(upper_A, upper_sum_b, sign=+1)
+        lb = _get_concrete_bound(lower_A, lower_sum_b, x, sign=-1)
+        ub = _get_concrete_bound(upper_A, upper_sum_b, x, sign=+1)
         if ub is None:
-            ub = x_U.new([np.inf])
+            ub = x.new([np.inf])
         if lb is None:
-            lb = x_L.new([-np.inf])
+            lb = x.new([-np.inf])
         return ub, lb
 
 
@@ -179,53 +177,64 @@ if __name__ == '__main__':
     print("Network prediction: {}".format(output))
 
     eps = 0.03
+    def print_bounds(lb, ub, batch_sz, y_sz):
+        for i in range(batch_sz):
+            for j in range(y_sz):
+                print('f_{j}(x_{i}): {l:8.4f} <= f_{j}(x_{i}+delta) <= {u:8.4f}'.format(
+                    j=j, i=i, l=lb[i][j].item(), u=ub[i][j].item()))
 
     print(f"Verifiying Pertubation - {eps}")
 
     print("----------------------------------------------------")
-    print("Auto_LiRPA CROWN method:")
 
     """Auto_LiRPA method"""
+    print("Auto_LiRPA CROWN method:")
     from auto_LiRPA.perturbations import PerturbationLpNorm
     from auto_LiRPA import BoundedModule, BoundedTensor
+    import warnings
+    warnings.filterwarnings('ignore')
+
     ptb = PerturbationLpNorm(eps=eps, norm=1., x_L=torch.zeros_like(x_test).float(), x_U=torch.ones_like(x_test).float())
     bounded_x = BoundedTensor(x_test, ptb)
     auto_lirpa_bounded_model = BoundedModule(model, torch.zeros_like(x_test))
     auto_lirpa_bounded_model.eval()
     with torch.no_grad():
         lirpa_lb, lirpa_ub = auto_lirpa_bounded_model.compute_bounds(x=(bounded_x,), method='CROWN')
-    for i in range(batch_size):
-        for j in range(y_size):
-            print('f_{j}(x_{i}): {l:8.4f} <= f_{j}(x_{i}+delta) <= {u:8.4f}'.format(
-                j=j, i=i, l=lirpa_lb[i][j].item(), u=lirpa_ub[i][j].item()))
+    print_bounds(lirpa_lb, lirpa_ub, batch_size, y_size)
     print("----------------------------------------------------")
     
-    print("CROWN on converted model")
-    """Converted model using CROWN"""
-    new_model = simplex_propagation_orig(model, x_test, eps)
-
-    x_u = torch.ones(1, 2*28*28)
-    x_l = torch.zeros(1, 2*28*28)
-
-    boundedmodel = BoundedSequential.convert(new_model)
-    ub, lb = boundedmodel.compute_bounds(x_U=x_u, x_L=x_l, upper=True, lower=True)
-    for i in range(batch_size):
-        for j in range(y_size):
-            print('f_{j}(x_{i}): {l:8.4f} <= f_{j}(x_{i}+delta) <= {u:8.4f}'.format(
-                j=j, i=i, l=lb[i][j].item(), u=ub[i][j].item()))
+    """Original model using CROWN"""
+    print("CROWN on original model")
+    boundedmodel = BoundedSequential.convert(model)
+    orig_crown_ub, orig_crown_lb = boundedmodel.compute_bounds(x=x_test, eps=eps)
+    print_bounds(orig_crown_lb, orig_crown_ub, batch_size, y_size)
     print("----------------------------------------------------")
 
-    print("Simplex method:")
-    """Simplex method"""
-    new_model = simplex_propagation(model, x_test, eps)
+    """Converted model using CROWN"""
+    print("CROWN on converted model")
+    new_model = simplex_propagation_orig(model, x_test, eps)
 
-    x_u = torch.ones(1, 2*28*28)
-    x_l = torch.zeros(1, 2*28*28)
+    x = torch.zeros(1, 2*28*28)
 
     boundedmodel = BoundedSequential.convert(new_model)
-    simplex_ub, _ = boundedmodel.compute_bounds(x_U=x_u, x_L=x_l, upper=True, lower=False)
-    for i in range(batch_size):
-        for j in range(y_size):
-            assert lb[i][j].item() <= simplex_ub[i][j].item()
-            print('f_{j}(x_{i}): {l:8.4f} <= f_{j}(x_{i}+delta) <= {u:8.4f}'.format(
-                j=j, i=i, l=lb[i][j].item(), u=simplex_ub[i][j].item()))
+    converted_crown_ub, converted_crown_lb = boundedmodel.compute_bounds(x=x, upper=True, lower=True, eps=1.)
+    print_bounds(converted_crown_lb, converted_crown_ub, batch_size, y_size)
+
+    # Verify converted model does not modify result and our own implemented l1 norm crown is same as auto_LiRPA's
+    # implementation.
+    assert torch.allclose(lirpa_lb, orig_crown_lb), "Our own implemented l1 norm CROWN has issue (lower bound)"
+    assert torch.allclose(orig_crown_lb, converted_crown_lb), "Converted model modify results on CROWN (lower bound)"
+    assert torch.allclose(lirpa_ub, orig_crown_ub), "Our own implemented l1 norm CROWN has issue (upper bound)"
+    assert torch.allclose(orig_crown_ub, converted_crown_ub), "Converted model modify results on CROWN (upper bound)"
+
+    print("----------------------------------------------------")
+
+    """Simplex method"""
+    print("Simplex method:")
+    new_model = simplex_propagation(model, x_test, eps)
+
+    x = torch.zeros(1, 2*28*28)
+
+    boundedmodel = BoundedSequential.convert(new_model)
+    simplex_ub, _ = boundedmodel.compute_bounds(x=x, upper=True, lower=False, eps=1.)
+    print_bounds(converted_crown_lb, simplex_ub, batch_size, y_size)
